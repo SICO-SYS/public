6506eadf32d8354ee65fdb2e9797817ca7f98ae7
SingleCorner <sinerwr@gmail.com>
2017-08-01 13:30:04 +0800

    add_string<->int

--- encoding.go
+++ encoding.go
@@ -3,6 +3,7 @@ package public
 import (
 	"encoding/base64"
 	"net/url"
+	"strconv"
 )
 
 func ToBase64(b []byte, needUrl bool) string {
@@ -10,4 +11,13 @@ func ToBase64(b []byte, needUrl bool) string {
 		return url.QueryEscape(base64.StdEncoding.EncodeToString(b))
 	}
 	return base64.StdEncoding.EncodeToString(b)
+}
+
+func Atoi(num string) int {
+	result, _ := strconv.Atoi(num)
+	return result
+}
+
+func Itoa(num int) string {
+	return strconv.Itoa(num)
 }
