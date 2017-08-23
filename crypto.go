4a6f4bd3d9b0f8f97a29d8f83251e39542216f3d
SingleCorner <sinerwr@gmail.com>
2017-08-23 17:47:01 +0800

    follow SICO-15

--- crypto.go
+++ crypto.go
@@ -21,10 +21,22 @@ func EncryptWithSha256(v string) string {
 	return hex.EncodeToString(hash.Sum(nil))
 }
 
+func EncryptBytesWithSha256(value []byte) []byte {
+	hash := sha256.New()
+	hash.Write(value)
+	return hash.Sum(nil)
+}
+
 func EncryptWithHmacSha256(k string, v string) string {
 	s := hmac.New(sha256.New, []byte(k))
 	s.Write([]byte(v))
 	return hex.EncodeToString(s.Sum(nil))
+}
+
+func EncryptBytesWithHmacSha256(key []byte, value string) []byte {
+	s := hmac.New(sha256.New, key)
+	s.Write([]byte(value))
+	return s.Sum(nil)
 }
 
 func Hmac256ToBase64(key string, str string, IsUrl bool) string {
@@ -49,4 +61,8 @@ func Hmac1ToBase64(key string, str string, IsUrl bool) string {
 	s := hmac.New(sha1.New, []byte(key))
 	s.Write([]byte(str))
 	return EncodingBase64(s.Sum(nil), IsUrl)
+}
+
+func EncodingBytesToHex(src []byte) string {
+	return hex.EncodeToString(src)
 }
--- time.go
+++ time.go
@@ -18,16 +18,6 @@ func Now() string {
 	return time.Now().Format("2006-01-02 15:04:05")
 }
 
-func TimesPer30s() (string, string, string) {
-	currentTimeStamp := time.Now().Unix()
-	currentTimes := int(math.Floor(float64(currentTimeStamp / 30)))
-	stringCurrentTimes := strconv.Itoa(currentTimes)
-	stringPrevTimes := strconv.Itoa(currentTimes - 1)
-	stringNextTimes := strconv.Itoa(currentTimes + 1)
-
-	return stringPrevTimes, stringCurrentTimes, stringNextTimes
-}
-
 func CurrentTimeStamp() string {
 	ts := time.Now().Unix()
 	s := strconv.FormatInt(ts, 10)
@@ -36,4 +26,21 @@ func CurrentTimeStamp() string {
 
 func CurrentUTCISO8601() string {
 	return time.Now().UTC().Format(time.RFC3339)
+}
+
+func CurrentYYYMMDD() string {
+	return time.Now().UTC().Format("20060102")
+}
+func CurrentYYYMMDDTHHMMSSZ() string {
+	return time.Now().UTC().Format("20060102T150405Z")
+}
+
+func TimesPer30s() (string, string, string) {
+	currentTimeStamp := time.Now().Unix()
+	currentTimes := int(math.Floor(float64(currentTimeStamp / 30)))
+	stringCurrentTimes := strconv.Itoa(currentTimes)
+	stringPrevTimes := strconv.Itoa(currentTimes - 1)
+	stringNextTimes := strconv.Itoa(currentTimes + 1)
+
+	return stringPrevTimes, stringCurrentTimes, stringNextTimes
 }
