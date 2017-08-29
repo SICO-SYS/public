c0af31234df10d97172fa3f316f3f56d78803443
SingleCorner <sinerwr@gmail.com>
2017-08-29 16:07:59 +0800

    add_string_int64_float64_convert

--- encoding.go
+++ encoding.go
@@ -18,6 +18,19 @@ func String2Int(num string) int {
 	return result
 }
 
+func String2Float(num string) float64 {
+	result, _ := strconv.ParseFloat(num, 64)
+	return result
+}
+
 func Int2String(num int) string {
 	return strconv.Itoa(num)
+}
+
+func Int642String(num int64) string {
+	return strconv.FormatInt(num, 10)
+}
+
+func Int2Float(num int) float64 {
+	return float64(num)
 }
